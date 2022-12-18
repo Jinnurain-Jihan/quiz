798f7fd5f2ff4a358cca46a2fb26cb87806e4444
jinnurain-jihan <zinnurainzihan171@gmail.com>
2022-12-18 19:43:40 +0600

    quiz

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>php quizer</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <p>PHP QUIZER</p>
+        </div>
+    </header>
+    <main>
+        <div class="menu">
+            <a href="main.php" class="btn">Take Quiz</a>
+            <a href="add.php" class="btn">Add Question</a>
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            Copyright &copy; Jinnurain Jihan
+        </div>
+    </footer>
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>php quizer</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <p>PHP QUIZER</p>
+        </div>
+    </header>
+    <main>
+        <div class="main">
+            <h2>Test Your Knowledge</h2>
+            <p>This is a multiple choise quiz to test your knowledge</p>
+            <ul>
+                <li><strong>Number of questions:</strong>5</li>
+                <li><strong>Type:</strong>Multiple choise</li>
+                <li><strong>Estimated Time:</strong>5 minutes</li>
+            </ul>
+            <a href="question.php?n=1">Start quiz</a>
+        </div>
+    </main>
+    <footer>
+        <div class="container">
+            Copyright &copy; Jinnurain Jihan
+        </div>
+    </footer>
+</body>
+</html>
